022f38158c05d3fa30079e889cf7d9899856c265
Jaime Redondo <jairegra@gmail.com>
2025-07-22 18:21:05 +0200

    Improve opcode handling and comments

--- src/emulator.zig
+++ src/emulator.zig
@@ -20,18 +20,27 @@ pub const Memory = struct {
     }
 };
 
-pub const Flag = enum(u8) {
-    C = 1 << 0, // Carry flag
-    Z = 1 << 1, // Zero flag
-    I = 1 << 2, // Interrupt disable
-    D = 1 << 3, // Decimal mode
-    B = 1 << 4, // Break command
-    U = 1 << 5, // Unused
-    V = 1 << 6, // Overflow flag
-    N = 1 << 7, // Negative flag
-};
-
 pub const CPU = struct {
+    // -------------------------------- CONSTANTS -------------------------------
+
+    pub const Flag = enum(u8) {
+        C = 1 << 0, // Carry flag
+        Z = 1 << 1, // Zero flag
+        I = 1 << 2, // Interrupt disable
+        D = 1 << 3, // Decimal mode
+        B = 1 << 4, // Break command
+        U = 1 << 5, // Unused
+        V = 1 << 6, // Overflow flag
+        N = 1 << 7, // Negative flag
+    };
+
+    pub const Opcode = enum(u8) {
+        LDA_IMM = 0xA9, LDA_ZPG = 0xA5, LDA_ZPX = 0xB5, LDA_ABS = 0xAD, LDA_ABX = 0xBD,
+        LDA_ABY = 0xB9, LDA_IDX = 0xA1, LDA_IDY = 0xB1,
+    };
+
+    // --------------------------------- FIELDS ---------------------------------
+
     pc: u16,            // Program counter
     sp: u8,             // Stack pointer
     a: u8,              // Accumulator
@@ -40,6 +49,8 @@ pub const CPU = struct {
     status: u8,         // Processor status flags
     memory: *Memory,    // RAM (only device connected to bus)
     cycles: u32,        // Cycles remaining for current instruction
+
+    // ------------------------------ CORE METHODS ------------------------------
 
     pub fn init(memory: *Memory) CPU {
         var cpu: CPU = undefined;
@@ -66,10 +77,16 @@ pub const CPU = struct {
 
     pub fn tick(self: *CPU) void {
         if (self.cycles == 0) {
-            const opcode = self.fetch();
-            const cycles_required = self.execute(opcode);
-            
-            self.cycles = cycles_required;
+            const fetched = self.fetch();
+            const maybe_opcode = decodeOpcode(fetched);
+
+            if (maybe_opcode) |opcode| {
+                const cycles_required = self.execute(opcode);
+                self.cycles = cycles_required;
+            } else {
+                std.debug.print("Unknown opcode: '0x{X:0>2}'\n", .{fetched});
+                return;
+            }
         }
 
         if (self.cycles > 0) self.cycles -= 1;
@@ -90,19 +107,37 @@ pub const CPU = struct {
         return byte;
     }
 
-    pub fn execute(self: *CPU, opcode: u8) u32 {
+    pub fn getFlag(self: *CPU, flag: Flag) bool {
+        return (self.status & @intFromEnum(flag) > 0);
+    }
+
+    pub fn setFlag(self: *CPU, flag: Flag, value: bool) void {
+        if (value) self.status |= @intFromEnum(flag)
+        else self.status &= ~(@intFromEnum(flag));
+    }
+
+    pub fn execute(self: *CPU, opcode: Opcode) u32 {
         return switch (opcode) {
-            0xA1 => self.testing(),
+            .LDA_IMM => self.lda_imm(),
             else => {
-                std.debug.print("Unimplemented opcode: '0x{X:0>2}'\n", .{opcode});
+                std.debug.print("Unimplemented opcode: '0x{X:0>2}'\n", .{@intFromEnum(opcode)});
                 return 0;
             }
         };
     }
 
-    pub fn testing(self: *CPU) u32 {
-        _ = self;
-        std.debug.print("Testing\n", .{});
-        return 3;
+    // ------------------------------ INSTRUCTIONS ------------------------------
+    // Reference: http://www.6502.org/users/obelisk/6502/reference.html
+
+
+
+    // ---------------------------- HELPER FUNCTIONS ----------------------------
+
+    fn decodeOpcode(byte: u8) ?Opcode {
+        return std.meta.intToEnum(Opcode, byte) catch null;
+    }
+
+    fn isBitSet(byte: u8, bit: u3) bool {
+        return (byte & (@as(u8, 1) << bit)) != 0;
     }
 };
